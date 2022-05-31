1228a90cc7cdaf3f16b307c90dffb4154c64184c
MDimas27 <mdimas.umb@gmail.com>
2022-05-31 09:20:58 +0700

    Menampilkan error validation

--- main.go
+++ main.go
@@ -1,35 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
-func main()  {
+func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
 
 	router.GET("/hello", helloHandler)
 
-	router.GET("/books/:id", booksHandler)	
+	router.GET("/books/:id", booksHandler)
 
 	router.GET("/query", queryHandler)
+
+	router.POST("/books", postBooksHandler)
 
 	router.Run()
 }
 
 func rootHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H {
+	c.JSON(http.StatusOK, gin.H{
 		"name": "Dimas Surya",
-		"bio": "Software Engineer and Remoter Worker",
+		"bio":  "Software Engineer and Remoter Worker",
 	})
 }
 
 func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H {
-		"title": "Belajar Golang",
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Belajar Golang",
 		"subtitle": "Belajar Golang bareng-bareng",
 	})
 }
@@ -42,7 +48,40 @@ func booksHandler(c *gin.Context) {
 
 func queryHandler(c *gin.Context) {
 	title := c.Query("title")
+	price := c.Query("price")
 
-	c.JSON(http.StatusOK, gin.H{"title": title})
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+
+}
+
+type BookInput struct {
+	Title string 			`json:"title" binding:"required"`
+	Price json.Number    	`json:"price" binding:"required,number"`
+}
+
+func postBooksHandler(c *gin.Context) {
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
 	
+		errorMessages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages  = append(errorMessages, errorMessage)
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+
+		return
+			
+	}
+
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+	})
 }
